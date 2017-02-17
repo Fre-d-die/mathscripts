adb8f05c89918cf4d53c052c7ef4555cf2935940
Freddie Bubble <freddiebubble@talktalk.net>
2017-02-17 10:36:05 +0000

    Made it more efficient

--- quadraticEquations.py
+++ quadraticEquations.py
@@ -2,11 +2,7 @@ import math
 a=int(input("what is the coefficient of x^2?"))
 b=int(input("what is the coefficeint of x?"))
 c=int(input("what is the last number?"))
-sqrtThis=(b*b)-(4*a)*(c)
-sqrtDone=math.sqrt(sqrtThis)
-bAdd=b+sqrtDone
-bMinus=b-sqrtDone
-answrAdd=str(bAdd/(a*2))
-answrMinus=str(bMinus/(a*2))
-print("x="+answrMinus)
+answrAdd=str((b+(math.sqrt((b*b)-(4*a)*(c))))/(2*a))
+answrMinus=str(str((b-(math.sqrt((b*b)-(4*a)*(c))))/(2*a)))
 print("x="+answrAdd)
+print("x="+answrMinus)
