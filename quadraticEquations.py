768e1aa0253642797dc0c8af46ffcabcfe9695dd
Freddie Bubble <freddiebubble@Freddies-MacBook-Pro.local>
2017-03-03 17:35:11 +0000

    added message for if number cannot be square rooted

--- quadraticEquations.py
+++ quadraticEquations.py
@@ -1,8 +1,13 @@
 import math
-a=int(input("what is the coefficient of x^2?"))
-b=int(input("what is the coefficeint of x?"))
-c=int(input("what is the last number?"))
-answrAdd=str((b+(math.sqrt((b**2)-(4*a)*(c))))/(2*a))
-answrMinus=str(str((b-(math.sqrt((b**2)-(4*a)*(c))))/(2*a)))
-print("x="+answrAdd)
-print("x="+answrMinus)
+a = int(input("what is the coefficient of x^2?"))
+b = int(input("what is the coefficeint of x?"))
+c = int(input("what is the last number?"))
+sqrtThis = ((b**2)-(4*a)*(c))/(2*a)
+if sqrtThis < 0:
+    print("the answer is not a real number")
+else:
+    sqrt = math.sqrt(((b**2)-(4*a)*(c))/(2*a))
+    pstv = str(b+sqrt)
+    ngtv = str(b-sqrt)
+    print("x=", pstv)
+    print("x=", ngtv)
